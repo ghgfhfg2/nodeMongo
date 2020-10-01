3274f67cd2ff91a4c3433c4cd9ef4bf816ea2701
ghgfhfg2 <ghgfhfg2@naver.com>
2020-10-01 17:40:14 +0900

    update

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const app = express();
 const port = 5000;
+const bodyParser = require('body-parser');
+
+const config = require('./config/key');
+
+const { User } = require('./models/User');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
 
 const monggoose = require('mongoose');
-monggoose.connect('mongodb+srv://sooya:!1gustn2486@cluster0.oflbb.mongodb.net/Cluster0?retryWrites=true&w=majority',{
+monggoose.connect(config.mongoURI,{
     useNewUrlParser:true,
     useUnifiedTopology:true,
     useCreateIndex:true,
@@ -14,7 +23,21 @@ monggoose.connect('mongodb+srv://sooya:!1gustn2486@cluster0.oflbb.mongodb.net/Cl
 
 
 app.get('/', function(req, res) {
-  res.send('hello world!!');
+  res.send('hello world!!1');
 });
+
+app.post('/register', (req,res) => {
+
+    
+    const user = new User(req.body);
+
+    user.save((err, userInfo) => {
+        if(err) return res.json({ success: false, err})
+        return res.status(200).json({
+            success: true
+        })
+    });
+
+})
 
 app.listen(port, ()=> console.log(`Example app listening on port ${port}!`))

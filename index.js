3e4d9e8ab32afd84393245157f31be7706da5e81
ghgfhfg2 <ghgfhfg2@naver.com>
2020-10-02 13:13:00 +0900

    update login

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const port = 5000;
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser')
 
 const config = require('./config/key');
 
@@ -9,6 +10,7 @@ const { User } = require('./models/User');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 
 const monggoose = require('mongoose');
@@ -38,6 +40,33 @@ app.post('/register', (req,res) => {
         })
     });
 
+})
+
+app.post('/api/users/login', (req,res) => {
+    User.findOne({ email: req.body.email }, (err, user) => {
+        if(!user){
+            return res.json({
+                loginSuccess: false,
+                message: "제공된 이메일에 해당하는 유저가 없습니다."
+            })
+        }
+        user.comparePassword( req.body.password, (err, isMatch) =>{
+            if(!isMatch)
+            return res.json({
+                loginSuccess: false,
+                massage: "비밀번호가 틀렸습니다."
+            })
+            user.generateToken((err, user)=>{
+                if(err) return res.status(400).send(err);
+                res.cookie("x_auth", user.token)
+                .status(200)
+                .json({
+                    loginSuccess:true,
+                    userId: user._id
+                })
+            })
+        })
+    })
 })
 
 app.listen(port, ()=> console.log(`Example app listening on port ${port}!`))
--- models/User.js
+++ models/User.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const jwt = require('jsonwebtoken');
 
 const userSchema = mongoose.Schema({
     name: {
@@ -14,6 +17,10 @@ const userSchema = mongoose.Schema({
         type: String,
         maxLength:50
     },
+    lastname: {
+        type: String,
+        minlnegth:5
+    },
     role: {
         type: Number,
         default: 0
@@ -26,6 +33,41 @@ const userSchema = mongoose.Schema({
         type: Number
     }
 })
+
+userSchema.pre('save',function( next ){
+    let user = this;
+    if(user.isModified('password')){    
+        bcrypt.genSalt(saltRounds, function(err, salt) {
+            if(err) return next(err)
+            bcrypt.hash(user.password, salt, function(err, hash) {
+                if(err) return next(err)
+                user.password = hash
+                next()
+            });
+        });
+    } else{
+        next()
+    }
+})
+
+userSchema.methods.comparePassword = function(plainPssword, cb){
+    bcrypt.compare(plainPssword, this.password, function(err, isMatch){
+        if(err) return cb(err),
+        cb(null, isMatch)
+    })
+}
+
+userSchema.methods.generateToken = function(cb) {
+    let user = this;
+    let token = jwt.sign(user._id.toHexString(), 'secretToken')
+        
+    user.token = token
+    user.save(function(err,user){
+        if(err) return cb(err)
+        cb(null, user)
+    })
+}
+
 
 const User = mongoose.model('User', userSchema)
 

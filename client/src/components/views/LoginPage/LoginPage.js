d8d7ff581f64d278ae21e13d87d93e0768c3b546
ghgfhfg2 <ghgfhfg2@naver.com>
2020-10-05 19:41:40 +0900

    update

--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -1,11 +1,58 @@
-import React from 'react';
+import Axios from 'axios';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import {loginUser} from '../../../_actions/user_action';
 
-function LoginPage() {
+function LoginPage(props) {
 
+    const dispatch = useDispatch();
+
+    const [Email, setEmail] = useState('')
+    const [passWord, setPassword] = useState('')
+
+    const onEmailHandler = (e) => {
+        setEmail(e.currentTarget.value)
+    }
+    const onPasswordHandler = (e) => {
+        setPassword(e.currentTarget.value)
+    }
+    const onSubmitHandler = (e) => {
+        e.preventDefault();
+
+        let body = {
+            email:Email,
+            password:passWord
+        }
+
+        dispatch(loginUser(body))
+            .then(response =>{
+                if(response.payload.loginSuccess){
+                    props.history.push('/')
+                }else{
+                    alert('Error')
+                }
+            })
+
+    }
     
     return (
-        <div>
-            LoginPage
+        <div style={{
+            display:'flex',justifyContent:'center',alignItems:'center',
+            width:'100%',height:'100vh'
+        }}>
+            <form onSubmit={onSubmitHandler} style={{
+                display:'flex',flexDirection:'column'
+            }}>
+                <label>Email</label>
+                <input type="email" value={Email} onChange={onEmailHandler} />
+                <label>password</label>
+                <input type="password" value={passWord} onChange={onPasswordHandler} />
+                <br />
+                <button type="submit">
+                    login
+                </button>
+
+            </form>
         </div>
     )
 }

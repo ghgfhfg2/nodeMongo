dd99002a1131bc3b2f2a3683dd7e1d53c7771dc0
hyunsoo kim <ghgfhfg2@naver.com>
2021-01-18 09:07:25 +0900

    .

---
+++ server/Check.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const checkSchema = mongoose.Schema({
+  id:{
+    type: String,
+  },
+  part:{
+    type: String,
+  },
+  name: {
+    type: String,
+    maxLength: 50,
+  },
+  role: {
+    type: Number,
+    default: 0,
+  },
+  date: {
+    type: Number,
+  },
+  day: {
+    type: String,
+  },
+  time: {
+    type: String,
+  },
+  check: {
+    type: String,
+  },
+  comment: {
+    type: String,
+  },
+});
+
+const Check = mongoose.model("Check", checkSchema);
+
+module.exports = { Check };
